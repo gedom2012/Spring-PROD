99186968cd794ef2024f6479ef26fcdc3ed4be21
Marcelo Mariath <gedom2012@gmail.com>
2020-03-02 21:58:06 +0000

    Imlementado Classe Categorias

--- src/main/java/com/mariath/spring/resources/CategoriaResource.java
+++ src/main/java/com/mariath/spring/resources/CategoriaResource.java
@@ -1,16 +1,28 @@
 package com.mariath.spring.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mariath.spring.domain.Categoria;
+
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
+
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST ESTA FUNCIONANDO";
+	public List<Categoria> listar() {
+
+		Categoria c1 = new Categoria(1, "Informatica");
+		Categoria c2 = new Categoria(2, "Escritório");
+
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(c1);
+		lista.add(c2);
+		return lista;
 	}
 
 }
