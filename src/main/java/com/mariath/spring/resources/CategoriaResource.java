bc6c83dd1309e589317d23d876ab68a16b14e8a1
Marcelo Mariath <gedom2012@gmail.com>
2020-03-03 23:55:24 +0000

    Criado Repository e Service para Categorias

--- src/main/java/com/mariath/spring/resources/CategoriaResource.java
+++ src/main/java/com/mariath/spring/resources/CategoriaResource.java
@@ -1,28 +1,26 @@
 package com.mariath.spring.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mariath.spring.domain.Categoria;
+import com.mariath.spring.services.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Categoria> listar() {
+	@Autowired
+	private CategoriaService service;
 
-		Categoria c1 = new Categoria(1, "Informatica");
-		Categoria c2 = new Categoria(2, "Escritório");
-
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(c1);
-		lista.add(c2);
-		return lista;
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria obj = service.find(id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
