5dc6c0501d5a1b4824e3dd570b9009aecb0463f9
Marcelo Mariath <gedom2012@gmail.com>
2020-03-04 23:25:20 +0000

    Implementado associacao entre classes produto e categorias

--- src/main/java/com/mariath/spring/ModeloConceitualNewApplication.java
+++ src/main/java/com/mariath/spring/ModeloConceitualNewApplication.java
@@ -8,14 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.mariath.spring.domain.Categoria;
+import com.mariath.spring.domain.Produto;
 import com.mariath.spring.repositories.CategoriaRepository;
+import com.mariath.spring.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class ModeloConceitualNewApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ModeloConceitualNewApplication.class, args);
 
@@ -23,10 +27,23 @@ public class ModeloConceitualNewApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
 
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 
 	}
 
