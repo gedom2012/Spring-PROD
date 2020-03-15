2eb6c0ce3eed5c5a17e2247d7f336b72ebb5efdc
Marcelo Mariath <gedom2012@gmail.com>
2020-03-15 17:47:35 +0000

    ItemPedido e Item PedidoPK (classe associativa)

--- src/main/java/com/mariath/spring/NewApplication.java
+++ src/main/java/com/mariath/spring/NewApplication.java
@@ -13,6 +13,7 @@ import com.mariath.spring.domain.Cidade;
 import com.mariath.spring.domain.Cliente;
 import com.mariath.spring.domain.Endereco;
 import com.mariath.spring.domain.Estado;
+import com.mariath.spring.domain.ItemPedido;
 import com.mariath.spring.domain.Pagamento;
 import com.mariath.spring.domain.PagamentoComBoleto;
 import com.mariath.spring.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.mariath.spring.repositories.CidadeRepository;
 import com.mariath.spring.repositories.ClienteRepository;
 import com.mariath.spring.repositories.EnderecoRepository;
 import com.mariath.spring.repositories.EstadoRepository;
+import com.mariath.spring.repositories.ItemPedidoRepositry;
 import com.mariath.spring.repositories.PagamentoRepository;
 import com.mariath.spring.repositories.PedidoRepository;
 import com.mariath.spring.repositories.ProdutoRepository;
@@ -48,6 +50,8 @@ public class NewApplication implements CommandLineRunner {
 	private PedidoRepository pedidoRepositpry;
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	@Autowired
+	private ItemPedidoRepositry itemPedidoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(NewApplication.class, args);
@@ -73,45 +77,59 @@ public class NewApplication implements CommandLineRunner {
 
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
-		
+
 		Estado est1 = new Estado(null, "Minas Gerais");
 		Estado est2 = new Estado(null, "São Paulo");
-		
+
 		Cidade c1 = new Cidade(null, "Uberlândia", est1);
 		Cidade c2 = new Cidade(null, "São Paulo", est2);
 		Cidade c3 = new Cidade(null, "Campinas", est2);
-		
+
 		est1.getCidade().addAll(Arrays.asList(c1));
-		est2.getCidade().addAll(Arrays.asList(c2, c3));		
-		
+		est2.getCidade().addAll(Arrays.asList(c2, c3));
+
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepositoty.saveAll(Arrays.asList(c1, c2, c3));
-		
+
 		Cliente cli1 = new Cliente(null, "Maria da Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
-		
+
 		Endereco e1 = new Endereco(null, "Rua FLores", "300", "Apto 203", "Jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Apto 203", "Jardim", "3877012", cli1, c2);
-		
-		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));		
-		
+
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
-		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");		
-		
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+
 		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), e1, cli1);
 		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), e2, cli1);
-		
+
 		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
 		ped1.setPagamento(pagto1);
-		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"),
+				null);
 		ped2.setPagamento(pagto2);
-		
+
 		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
-		 
+
 		pedidoRepositpry.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 
 	}
 
