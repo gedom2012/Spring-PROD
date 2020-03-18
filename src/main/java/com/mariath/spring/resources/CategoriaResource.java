fc33a6e9cffc1c49b779ceeea853c1501ea6a98e
Marcelo Mariath <gedom2012@gmail.com>
2020-03-18 09:50:35 +0000

    Implementado metodo DELETEem categorias e implementado nova exception personalizada

--- src/main/java/com/mariath/spring/resources/CategoriaResource.java
+++ src/main/java/com/mariath/spring/resources/CategoriaResource.java
@@ -43,5 +43,11 @@ public class CategoriaResource {
 		
 	}
 	
+	@RequestMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Integer id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
 
 }
--- src/main/java/com/mariath/spring/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/mariath/spring/resources/exceptions/ResourceExceptionHandler.java
@@ -7,15 +7,22 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.mariath.spring.services.exceptions.DataIntegrityException;
 import com.mariath.spring.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
-	
+
 	@ExceptionHandler(ObjectNotFoundException.class)
-	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
 		StandardError er = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(er);
+	}
+
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
+		StandardError er = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(er);
 	}
 
 }
