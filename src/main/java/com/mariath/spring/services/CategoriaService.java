3b51dc4433bcce16e4ca84d882583c392b2b410b
Marcelo Mariath <gedom2012@gmail.com>
2020-03-18 00:16:33 +0000

    Implementado Service PUT em Categorias

--- src/main/java/com/mariath/spring/services/CategoriaService.java
+++ src/main/java/com/mariath/spring/services/CategoriaService.java
@@ -19,10 +19,14 @@ public class CategoriaService {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado...:Id: " + id + ", Tipo: " + 
 		Categoria.class.getName()));
-}
-	
+}	
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
 		return repo.save(obj);
 	}
 	
