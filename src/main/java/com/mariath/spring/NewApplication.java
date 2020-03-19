d21c01104d4009bc6ea02d85fdaa894d4d5d338f
Marcelo Mariath <gedom2012@gmail.com>
2020-03-19 23:23:24 +0000

    Criado metodo GET por paginacao e demais parametros

--- src/main/java/com/mariath/spring/NewApplication.java
+++ src/main/java/com/mariath/spring/NewApplication.java
@@ -63,6 +63,11 @@ public class NewApplication implements CommandLineRunner {
 
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
+		Categoria cat3 = new Categoria(null, "Cama e mesa");
+		Categoria cat4 = new Categoria(null, "Otica");
+		Categoria cat5 = new Categoria(null, "Farmacia");
+		Categoria cat6 = new Categoria(null, "Para ti");
+		Categoria cat7 = new Categoria(null, "Para mim");
 
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
@@ -75,7 +80,7 @@ public class NewApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 
 		Estado est1 = new Estado(null, "Minas Gerais");
